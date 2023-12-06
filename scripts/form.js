7d3ebcfe30f3f4710ad44ea21902785c42eb74da
Christopher Petrinovich <petrinovichch@gmail.com>
2023-12-06 16:57:12 -0700

    Form almost done

--- scripts/form.js
+++ scripts/form.js
@@ -8,13 +8,10 @@ function checkSame() {
 	if (secret.value !== secretRepeated.value) {
 		message.textContent = "❗Passwords DO NOT MATCH!";
 		message.style.visibility = "show";
-		secretRepeated.style.backgroundColor = "#fff0f3";
 		secretRepeated.value = "";
         secret.value = "";
 		secret.focus();
 	} else {
 		message.style.display = "none";
-		secretRepeated.style.backgroundColor = "#fff";
-		secretRepeated.style.color = "#000";
 	}
 }
